c476f426acd6fcab70cba71778ffab949c3f04c3
Scott Runnels <srunnels@gmail.com>
2026-06-29 22:36:50 -0400

    typo fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@ outputs = {nixpkgs, home-manager, ... }:
   let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; }
+    pkgs = import nixpkgs { inherit system; };
   in {
     homeConfigurations = {
       myprofile = home-manager.lib.homeManagerConfiguration { 

1f656b7b950a32c2aa22fae691c1b676707c6e7e
Scott Runnels <srunnels@gmail.com>
2026-06-29 22:43:10 -0400

    Add flake.lock

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
 
     home-manager = {
-      url = "github:nix-community/home-manager-26.05";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpgks.follows = "nixpkgs"; 
 
     };
